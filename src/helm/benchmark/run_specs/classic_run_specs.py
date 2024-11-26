65fe952e681b27652f0596f58dab07fdf524863b
lucas-s-p <lucas.pereira@ccc.ufcg.edu.br>
2024-11-26 14:24:10 -0300

    remove sequence tokens

--- src/helm/benchmark/run_specs/classic_run_specs.py
+++ src/helm/benchmark/run_specs/classic_run_specs.py
@@ -613,8 +613,6 @@ def get_code_spec(dataset: str, timeout=3) -> RunSpec:
     if dataset == "humaneval":
         adapter_spec = get_completion_adapter_spec(
             temperature=0.2,
-            # Taken from the original OpenAI paper to prevent the further generation of irrelevant classes/functions
-            stop_sequences=[],
             max_tokens=600,
         )
     else:  # apps.
